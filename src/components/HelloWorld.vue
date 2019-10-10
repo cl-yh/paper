e634685669f0e82c9610729a1bac0f29b11c415b
1764564459 <1764564459@qq.com>
2019-10-10 18:15:26 +0800

    还差一点

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="hello">
+        <!-- 首页 -->
         <div class="paer-Herder">
             <img src="../assets/logo.png" alt />
             <div class="her-Column">
@@ -23,6 +24,7 @@
                 </div>
             </div>
         </div>
+
         <div class="swiper-container">
             <div class="swiper-wrapper">
                 <div class="swiper-slide icon-Img1 paer-Img1">
@@ -60,6 +62,7 @@
             <!-- 如果需要分页器 -->
             <div class="swiper-pagination icon-podd"></div>
         </div>
+<!-- 服务项目 -->
         <div class="paer-serve">
             <div class="serv-top">
                 <div class="serv-cata"></div>
@@ -71,7 +74,7 @@
                     <i class="two"></i>
                     <div class="sere-imgAll sere-img1"></div>
                     <div class="sere-boop">本硕写作</div>
-                    <div class="">
+                    <div class="sere-dis">
                         硕博导师团队<br/>
                         一对一服务
                     </div>
@@ -81,7 +84,7 @@
                     <i class="two"></i>
                      <div class="sere-imgAll sere-img2"></div>
                     <div class="sere-boop">意见修改</div>
-                    <div>
+                    <div class="sere-dis">
                         专业改内容意见<br/>
                         保证质量免费售后
                     </div>
@@ -91,7 +94,7 @@
                     <i class="two"></i>
                     <div class="sere-imgAll sere-img3"></div>
                     <div class="sere-boop">改重降重</div>
-                    <div>
+                    <div class="sere-dis">
                         降低重复率<br/>
                         不通过退款
                     </div>
@@ -101,7 +104,7 @@
                     <i class="two"></i>
                     <div class="sere-imgAll sere-img4"></div>
                     <div class="sere-boop">格式排版</div>
-                    <div>
+                    <div class="sere-dis">
                         按要求格式调整<br/>
                         内容布局排版
                     </div>
@@ -111,7 +114,7 @@
                     <i class="two"></i>
                     <div class="sere-imgAll sere-img5"></div>
                     <div class="sere-boop">外文翻译</div>
-                    <div>
+                    <div class="sere-dis">
                         目录摘要翻译<br/>
                         多国翻译中英互译
                     </div>
@@ -121,7 +124,7 @@
                     <i class="two"></i>
                     <div class="sere-imgAll sere-img6"></div>
                     <div class="sere-boop">PPT制作</div>
-                    <div>
+                    <div class="sere-dis">
                         制作答辩PPT<br/>
                         简历制作定制设计
                     </div>
@@ -131,7 +134,7 @@
                     <i class="two"></i>
                     <div class="sere-imgAll sere-img7"></div>
                     <div class="sere-boop">职称评审</div>
-                    <div>
+                    <div class="sere-dis">
                         高通过率<br/>
                         免费协助解答
                     </div>
@@ -141,13 +144,149 @@
                     <i class="two"></i>
                     <div class="sere-imgAll sere-img8"></div>
                     <div class="sere-boop">学术不端查重</div>
-                    <div>
+                    <div class="sere-dis">
                         正品支持验证<br/>
                         与学校结果一致
                     </div>
                 </li>
             </ul>
+        </div>
+<!--  我们的优势-->
+        <div class="paer-advantage">
+            <div class="paer-Tageto">
+                <div class="paer-TageImg"></div>
+                <div class="paer-mytage">我们的优势</div>
+            </div>
+            <div class="paer-tagenr">
+                <div class="paer-tagepo">
+                    <ul class="paer-tagemo">
+                        <li class="tuan"></li>
+                        <li class="dui"></li>
+                        <li class="shi"></li>
+                        <li class="li"></li>
+                    </ul>
+                    <div class="paer-fuwu">
+                        八年写作、修改经验<br>638位硕博学历老师<br>遍布12个一级学科<br>469个二级学科<br>专业，高效，安全<br>已成功为数万用户提供写作服务
+                    </div>
+                </div>
+               <div class="paer-tagepo">
+                    <ul class="paer-tagemo">
+                        <li class="fu"></li>
+                        <li class="wu"></li>
+                        <li class="zong"></li>
+                        <li class="zhi"></li>
+                    </ul>
+                    <div class="paer-fuwu">
+                        用户是上帝<br>兑现用户的诉求为第一要务！<br>以优惠的价格，提供优质的服务<br>纯人工降重，写作,保质/保量<br>按时、按约定要求完成任务。<br>保通过，不过退款<br>
+                    </div>
+                </div>
+               <div class="paer-tagepo">
+                    <ul class="paer-tagemo">
+                        <li class="jiang"></li>
+                        <li class="zhong"></li>
+                        <li class="biao"></li>
+                        <li class="zhun"></li>
+                    </ul>
+                    <div class="paer-fuwu">
+                        原创度高<br>设计新颖<br>文章结构、原义不变<br>高质量<br>语句通顺<br>语意不变，保证重复率达标。
+                    </div>
+                </div>
+                <div class="paer-tagepo">
+                    <ul class="paer-tagemo">
+                        <li class="shou"></li>
+                        <li class="hou"></li>
+                        <li class="fuOne"></li>
+                        <li class="wuOne"></li>
+                    </ul>
+                    <div class="paer-fuwu">
+                        一对一服务<br>7*18小时随时在线<br>按时交稿<br>高效高通过率<br>不满意可免费改<br>不通过无条件退款
+                    </div>
+                </div>
+               <div class="paer-tagepo">
+                    <ul class="paer-tagemo">
+                        <li class="an"></li>
+                        <li class="quan"></li>
+                        <li class="cheng"></li>
+                        <li class="luo"></li>
+                    </ul>
+                    <div class="paer-fuwu">
+                        加密操作<br>保护隐私<br>任务完成即删除底稿<br>不泄露信息<br>充分保障用户权益。<br>高兴而来满意而归。
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- 服务流程 -->
+        <div class="paer-flow">
+            <div class="flow-heider">
+                <div class="flow-ImgOne"></div>
+                <div class="flow-her">服务流程</div>
+            </div>
+            <ul class="flow-ul">
+                <li class="flow-li">
+                    <i class="flow-one"></i>
+                    <i class="flow-two"></i>
+                    <div class="flow-img flow-serviceImg"></div>
+                    <div class="flow-needAll">沟通需求</div>
+                    <div class="flow-anaAll">
+                        联系客服<br>确认需求
+                    </div>
+                </li>
+                 <li class="flow-li">
+                    <i class="flow-one"></i>
+                    <i class="flow-two"></i>
+                    <div class="flow-img flow-gujImg"></div>
+                    <div class="flow-needAll">免费估价</div>
+                    <div class="flow-anaAll">
+                        根据需求<br>免费估价
+                    </div>
+                </li>
+                 <li class="flow-li">
+                    <i class="flow-one"></i>
+                    <i class="flow-two"></i>
+                    <div class="flow-img flow-feiyImg"></div>
+                    <div class="flow-needAll">支付费用</div>
+                    <div class="flow-anaAll">
+                        协商价格<br>担保交易
+                    </div>
+                </li>
+                 <li class="flow-li">
+                    <i class="flow-one"></i>
+                    <i class="flow-two"></i>
+                    <div class="flow-img flow-jiaogImg"></div>
+                    <div class="flow-needAll">如约交稿</div>
+                    <div class="flow-anaAll">
+                        按照约定<br>完美交稿
+                    </div>
+                </li>
+                 <li class="flow-li">
+                    <i class="flow-one"></i>
+                    <i class="flow-two"></i>
+                    <div class="flow-img flow-shouhImg"></div>
+                    <div class="flow-needAll">完美售后</div>
+                    <div class="flow-anaAll">
+                        免费修改<br>通过为止
+                    </div>
+                </li>
+            </ul>
+        </div>
+<!-- 免费估价 -->
+        <div class="paer-evaluate">
 
+        </div>
+        <!-- 联系方式 -->
+        <div class="paer-relation">
+            <div class="rela-all">
+                <div class="rela-top">
+                    <div class="rela-dis rela-size"><img src="../assets/01.png" alt=""></div>
+                    <div class="rela-dis rela-left">
+                        <div class="rela-dh">18216191569</div>
+                        <div class="rela-site">地址：市中区1173 <br/>微信：18216191569<br/>邮箱：1543536379@qq.com</div>
+                    </div>
+                </div>
+                <div class="rela-bottom">
+                    <div class="rela-img"></div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -181,6 +320,366 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.rela-left{
+    margin-left:10px;
+}
+.rela-site{
+    margin-top: 8px;
+    color: #666;
+    line-height: 26px;
+    margin: 0 0 10px;
+}
+.rela-size{
+    width: 150px;
+    height:150px;
+}
+.rela-size img{
+width: 100%;
+height:100%;
+}
+.rela-dis{
+    float: left;
+}
+.rela-dh{
+    display: block;
+    margin: 10px 0px 15px 0px;
+    width: 243px;
+    height: 37px;
+    text-decoration: none;
+    font-size: 30px;
+    color: #2ED0B4;
+    font-weight: bold;
+}
+.rela-img{
+    display: block;
+    position: relative;
+    margin: 0 auto;
+    width: 424px;
+    height: 86px;
+    background: url('../assets/words.png') no-repeat 0px -787px;
+    -webkit-transition: all .6s ease-in-out 0s;
+    -moz-transition: all .6s ease-in-out 0s;
+    -ms-transition: all .6s ease-in-out 0s;
+    -o-transition: all .6s ease-in-out 0s;
+    transition: all .6s ease-in-out 0s;
+}
+.rela-bottom{
+    position: relative;
+    padding-top: 50px;
+    width: 100%;
+    border-top: 1px dotted #ccc;
+}
+.rela-top{
+   position: relative;
+    width: 100%;
+    height: 200px;
+    -webkit-transition: all .6s ease-in-out 0s;
+    -moz-transition: all .6s ease-in-out 0s;
+    -ms-transition: all .6s ease-in-out 0s;
+    -o-transition: all .6s ease-in-out 0s;
+    transition: all .6s ease-in-out 0s;
+    padding-top: 250px;
+}
+   
+.rela-all{
+    position: relative;
+    margin:0 auto;
+    width: 810px;
+    height: 330px;
+    -webkit-transition: all .3s ease-in-out 0s;
+    -moz-transition: all .3s ease-in-out 0s;
+    -ms-transition: all .3s ease-in-out 0s;
+    -o-transition: all .3s ease-in-out 0s;
+    transition: all .3s ease-in-out 0s;
+    
+}
+.paer-relation{
+    font-size: .1rem;
+    width:100%;
+    height:100vh;
+    position: relative;
+    background: url('../assets/wallpaper8.jpg') 50% bottom;
+    -moz-background-size: cover;
+    background-size: cover;
+}
+.paer-evaluate{
+    width: 100%;
+    height:100vh;
+    background: url('../assets/wallpaper4.jpg') 50% 90%;
+}
+.flow-anaAll{
+    line-height: 20px;
+    text-align: center;
+}
+.flow-needAll{
+    display: block;
+    margin: 200px 0px 10px 0px;
+    font-size: 16px;
+    text-align: center;
+    -webkit-transition: all .5s ease-in-out 0s;
+    -moz-transition: all .5s ease-in-out 0s;
+    -ms-transition: all .5s ease-in-out 0s;
+    -o-transition: all .5s ease-in-out 0s;
+    transition: all .5s ease-in-out 0s;
+    margin-top: 50px;
+    font-weight: 700;
+}
+.flow-img{
+     width: 72px;
+    display: block;
+    position: relative;
+    margin: 0 auto;
+    margin-top: 28px;
+    height: 70px;
+}
+.flow-serviceImg{
+    background: url('../assets/words.png') no-repeat -516px -630px;
+}
+.flow-feiyImg{
+    background: url('../assets/words.png') no-repeat -517px -704px;
+}
+.flow-gujImg{
+   background: url('../assets/words.png') no-repeat -596px -631px;
+}
+.flow-jiaogImg{
+    background: url('../assets/words.png') no-repeat -594px -708px;
+}
+.flow-shouhImg{
+    background: url('../assets/words.png') no-repeat -514px -793px;
+}
+.flow-one{
+    clip:rect(0px, 128px, -10px, 64px);
+}
+.flow-li:hover .flow-one{
+    clip:rect(0px,128px,128px,64px);
+}
+.flow-two{
+    clip:rect(128px,64px,128px,0px);
+}
+.flow-li:hover .flow-two{
+    clip:rect(0px,64px,128px,0px);
+}
+.flow-li i{
+    display: block;
+    position: absolute;
+    width: 128px;
+    height: 128px;
+    background: url('../assets/words.png') no-repeat 0px -624px;
+    -webkit-transition: all .5s ease-in-out 0s;
+    -moz-transition: all .5s ease-in-out 0s;
+    -ms-transition: all .5s ease-in-out 0s;
+    -o-transition: all .5s ease-in-out 0s;
+    transition: all .5s ease-in-out 0s;
+}
+.flow-li{
+    width: 128px;
+    height: 230px;
+    margin: 0 20px;
+    overflow: hidden;
+    display: inline-block;
+    padding-right: 5px;
+    padding-left: 5px;
+    list-style: none;
+}
+.flow-ul{
+    width: 912px;
+    margin: 0 auto;
+    position: relative;
+    z-index: 10;
+    padding-left: 0;
+    list-style: none;
+}
+.flow-ul{
+    margin-top: 30px;
+}
+.flow-heider{
+    padding-top:230px;
+    position: relative;
+    width: 100%;
+    text-align: center;
+    -webkit-transition: all .5s ease-in-out 0s;
+    -moz-transition: all .5s ease-in-out 0s;
+    -ms-transition: all .5s ease-in-out 0s;
+    -o-transition: all .5s ease-in-out 0s;
+    transition: all .5s ease-in-out 0s;
+    z-index: 10;
+}
+.flow-her{
+    padding-left: 5px;
+    font-size: 24px;
+}
+.flow-ImgOne{
+    display: block;
+    margin: 0 auto;
+    margin-bottom: 20px;
+    width: 240px;
+    height: 37px;
+    background: url('../assets/words.png') no-repeat -262px -576px;
+   
+}
+.paer-flow{
+    font-size: .1rem;
+    width:100%;
+    height: 100vh;
+    background: url('../assets/wallpaper6.jpg') 50% 45% no-repeat;
+}
+.paer-fuwu{
+    font-size: 14px;
+    color: #e1e1e1;
+    text-align: center;
+    line-height: 36px;
+    margin: 0 0 10px;
+}
+.tuan{
+    background: url('../assets/icons1.png') no-repeat -3px -7px;
+}
+.dui{
+    background: url('../assets/icons1.png') no-repeat -43px -7px;
+}
+.shi{
+    background: url('../assets/icons1.png') no-repeat -83px -7px;
+}
+.li{
+    background: url('../assets/icons1.png') no-repeat -123px -7px;
+}
+.fu{
+    background: url('../assets/icons1.png') no-repeat -163px -7px;
+}
+.wu{
+    background: url('../assets/icons1.png') no-repeat -603px -7px;
+}
+.zong{
+    background: url('../assets/icons1.png') no-repeat -243px -7px;
+}
+.zhi{
+    background: url('../assets/icons1.png') no-repeat -283px -7px;
+}
+.jiang{
+    background: url('../assets/icons1.png') no-repeat -323px -7px;
+}
+.zhong{
+    background: url('../assets/icons1.png') no-repeat -363px -7px;
+}
+.biao{
+    background: url('../assets/icons1.png') no-repeat -403px -7px;
+}
+.zhun{
+    background: url('../assets/icons1.png') no-repeat -443px -7px;
+}
+.shou{
+    background: url('../assets/icons1.png') no-repeat -483px -7px;
+}
+.hou{
+    background: url('../assets/icons1.png') no-repeat -523px -7px;
+}
+.fuOne{
+    background: url('../assets/icons1.png') no-repeat -163px -7px;
+}
+.wuOne{
+    background: url('../assets/icons1.png') no-repeat -603px -7px;
+}
+.an{
+    background: url('../assets/icons1.png') no-repeat -643px -7px;
+}
+.quan{
+    background: url('../assets/icons1.png') no-repeat -683px -7px;
+}
+.cheng{
+    background: url('../assets/icons1.png') no-repeat -723px -7px;
+}
+.luo{
+    background: url('../assets/icons1.png') no-repeat -763px -7px;
+}
+.paer-tagemo{
+    width: 248px;
+    margin: 0 auto;
+    margin-bottom: 20px;
+    padding-left: 0;
+    margin-left: -5px;
+    list-style: none;
+    
+}
+.paer-tagemo li{
+    position: relative;
+    margin: 0 12px;
+    padding: 0;
+    overflow: hidden;
+   
+}
+.paer-tagenr li{
+    width: 32px;
+    height: 32px;
+    display: inline-block;
+}
+   
+.paer-tagenr{
+    margin: 0 auto;
+    margin-top: 120px;
+    max-width: 1440px;
+}
+.paer-tagepo{
+    position: relative;
+    width: 240px;
+    margin: 0 20px;
+    float: left;
+    -webkit-box-sizing: content-box;
+    -moz-box-sizing: content-box;
+    box-sizing: content-box;
+}
+    
+.paer-mytage{
+    padding-left: 5px;
+    color: White;
+    font-size: 24px;
+}
+.paer-Tageto{
+    padding-top: 160px;
+    text-align:center;
+}
+.paer-TageImg{
+    display: block;
+    margin: 0 auto;
+    margin-bottom: 20px;
+    width: 240px;
+    height: 37px;
+    background: url('../assets/words.png') no-repeat 0px -576px;
+}
+.paer-advantage{
+    font-size: .1rem;
+    width:100%;
+    height:100vh;
+    background: url('../assets/wallpaper5.jpg') 50% 10% no-repeat
+}
+.sere-dis{
+    position: relative;
+    top: 200px;
+    color: White;
+    font-family: Arial, "宋体";
+    font-size: 12px;
+    line-height: 20px;
+    text-align: center;
+    margin: 0 0 10px;
+}
+.sere-li:hover .sere-dis{
+    top: 20px;
+    -webkit-transition: all .3s ease-in-out .5s;
+    -moz-transition: all .3s ease-in-out .5s;
+    -ms-transition: all .3s ease-in-out .5s;
+    -o-transition: all .3s ease-in-out .5s;
+    transition: all .3s ease-in-out .5s;
+}
+.one{
+    clip: rect(0px,156px,156px,78px)
+}
+.sere-li:hover .one{
+    clip: rect(0px, 156px, -10px, 78px)
+}
+.two{
+    clip:rect(0px,78px,156px,0px)
+}
+.sere-li:hover .two{
+    clip:rect(156px,78px,156px,0px)
+}
 .sere-boop{
     display: block;
     margin-top: 70px;
@@ -188,6 +687,15 @@ export default {
     font-weight: normal;
     text-align: center;
 }   
+.sere-li:hover .sere-boop{
+    color: White;
+    -webkit-transition-delay: .4s;
+    -moz-transition-delay: .4s;
+    -ms-transition-delay: .4s;
+    -o-transition-delay: .4s;
+    transition-delay: .4s;
+}
+   
 .sere-imgAll{
     display: block;
     position: relative;
@@ -238,7 +746,7 @@ export default {
 .sere-img6{
     background:url("../assets/words.png") no-repeat -583px -364px
 }
- .sere-li:hover .sere-img6{
+.sere-li:hover .sere-img6{
     background: url("../assets/words.png") no-repeat -495px -364px
 }
 .sere-img7{
@@ -278,7 +786,7 @@ export default {
     -o-transition: all .5s ease-in-out .3s;
     transition: all .5s ease-in-out .3s;
 }
-i{
+.sere-li i{
     display: block;
     position: absolute;
     width: 156px;
@@ -366,7 +874,6 @@ i{
     transition: all .5s linear 0s;
     z-index: 10;
 }
-
 .paer-acti2{
     display: block;
     width: 339px;
@@ -409,7 +916,6 @@ i{
     font-size: 0.11rem /* 16/75 */;
     line-height: 0.15rem /* 30/75 */;
     text-align: center;
-    -moz-opacity: 0;
     -webkit-transition: all 0.6s ease-in-out 0.3s;
     -moz-transition: all 0.6s ease-in-out 0.3s;
     -ms-transition: all 0.6s ease-in-out 0.3s;
@@ -473,8 +979,7 @@ a {
     height: 100vh;
 }
 .icon-Img1 {
-    background: url("http://www.paperred.cn/templets/Images/wallpaper1_3.jpg") 0
-        no-repeat;
+    background: url("http://www.paperred.cn/templets/Images/wallpaper1_3.jpg") 0 no-repeat;
     background-size: 100% 100%;
 }
 .icon-Img2 {
@@ -517,7 +1022,7 @@ a {
     }
 }
 </style>
- <style>
+<style>
 .icon-podd .swiper-pagination-bullet {
     width: 0.25rem /* 30/75 */;
     height: 0.025rem /* 8/75 */;
